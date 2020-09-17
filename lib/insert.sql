323aaf6e8c6c7209b6bdd64e6d9810dcc741fb1f
Arif Cagirici <acagirici23@gmail.com>
2020-09-17 03:43:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "Male", "Black", "calm", )
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "Male", "Black", "calm", 1); 
