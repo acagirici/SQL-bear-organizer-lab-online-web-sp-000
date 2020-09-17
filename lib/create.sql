fe4d250ae7602fc44af93fa859fa3cf042623533
Arif Cagirici <acagirici23@gmail.com>
2020-09-17 03:33:03 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
-id INTEGER, 
-name TEXT,
-age INTEGER,
-gender TEXT,
-color TEXT,
-temperament TEXT,
-alive NUMERIC,
+  id INTEGER, 
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive NUMERIC,
 );
